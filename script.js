b014b2ec426e14ff3224cb991e9ca1bf2594e8b8
JJCSL <71199058+JJCSL@users.noreply.github.com>
2020-09-20 10:07:31 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -16,3 +16,24 @@ function reveal(){
     document.getElementById("hiddenmessage").style.display = 'block';
 
 }
+
+function newItem(){
+    var item = document.getElementById("input").value;
+    var ul = document.getElementById("list");
+    var li = document.createElementById("li");
+
+    li.appendChild(document.createTextNode("-"+ item));
+    ul.appendChild(li);
+    document.getElementById("input").value = "";
+    li.onclick = removeitem;
+}
+
+document.body.onkeyup = function(e){
+    if (e.keyCode == 13){
+        newItem();
+    }
+}
+
+function removeItem(){
+    e.target.parentElement.removeChild(e.target);
+}
